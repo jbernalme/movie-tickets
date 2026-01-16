28c58e7e205db17a730e32e5642101bbfb9935cc
Jesus David Bernal <jbernalme@gmail.com>
2026-01-16 07:56:31 -0500

    feat: Add palette colors and logo app

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,12 +3,16 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="-rotate-6">
+                <AppLogoIcon className="size-10 fill-primary stroke-background" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+
+            <div className="ml-0.5 grid flex-1 text-left font-bebas-neue text-2xl">
+                <span className="mb-0.5 truncate leading-tight">
+                    Movie
+                    <span className="ml-0.5 font-extrabold text-primary">
+                        Tikets
+                    </span>
                 </span>
             </div>
         </>
